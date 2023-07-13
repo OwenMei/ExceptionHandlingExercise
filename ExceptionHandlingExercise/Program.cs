2183d471acae8178e6cab7b71e7b1e63622baeb8
owen mei <owenzackmei@gmail.com>
2023-07-13 11:20:16 -0400

    init commit

--- ExceptionHandlingExercise/Program.cs
+++ ExceptionHandlingExercise/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq.Expressions;
 
 namespace ExceptionHandlingExercise
 {
@@ -10,10 +11,18 @@ namespace ExceptionHandlingExercise
         {
             // -----------------------------------------------------------------------------
             // First create an char[], it must contain 6 numbers and 3 letters - name it arr
+            char[] arr = new char[] { '1','2','3','4','5','6','a','b','c'};
             // Create a list called numbers that will hold integers
+            List<int> numbers = new List<int>();
             // Create an string variable with an empty string initializer - name it str
-
+            string str;
             // using a foreach loop, attempt to parse the elements in your char[] with int.Parse()
+            /*
+            foreach(char c in arr)
+            {
+                int.Parse(c.ToString());
+            }
+            */
             // and Exceptions will be thrown 
             // Below we will set this up 
             // ------------------------------------------------------------------------------
@@ -23,7 +32,20 @@ namespace ExceptionHandlingExercise
             //TODO START HERE:
             
             // Make a foreach loop to iterate through your character array
-            
+            foreach(char c in arr)
+            {
+                try
+                {
+                    str = c.ToString();
+                    int number = int.Parse(str);
+                    numbers.Add(number);
+                }
+                catch
+                {
+                    Console.WriteLine($"Unable to Parse '{c}'");
+                }
+
+            }
                 // Now create a try catch
                 
                 
@@ -39,10 +61,10 @@ namespace ExceptionHandlingExercise
                 
             
 
-            //foreach (var num in numbers)
-            //{
-            //    Console.WriteLine(num);
-            //}
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
